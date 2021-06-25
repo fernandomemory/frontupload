a70fca90a31459e6c5b3afcac8185d4de19eccce
Fernando Fernandes <fernandofitilan@hotmail.com>
2021-06-25 14:55:25 -0300

    up

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Download das imagens';
+  description = 'Verifique logo em baixo';
+}
