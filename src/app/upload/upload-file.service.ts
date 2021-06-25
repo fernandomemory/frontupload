3a7381ccde0ad3d1a41e126aa713970ec13021c3
Fernando Fernandes <fernandofitilan@hotmail.com>
2021-06-25 17:36:18 -0300

    up

--- src/app/upload/upload-file.service.ts
+++ src/app/upload/upload-file.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpEvent, HttpRequest } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpEvent, HttpRequest,  HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError  } from 'rxjs';
+import { retry, catchError } from 'rxjs/operators';
+
 
 @Injectable({
   providedIn: 'root'
@@ -8,6 +10,31 @@ import { Observable } from 'rxjs';
 export class UploadFileService {
 
   constructor(private http: HttpClient) { }
+
+   // Http Options
+   httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  }
+
+  // Handle API errors
+  handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error.message);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong,
+      console.error(
+        `Backend returned code ${error.status}, ` +
+        `body was: ${error.error}`);
+    }
+    // return an observable with a user-facing error message
+    return throwError(
+      'Something bad happened; please try again later.');
+  };
+
 
   pushFileToStorage(file: File): Observable<HttpEvent<{}>> {
     const formdata: FormData = new FormData();
@@ -24,5 +51,14 @@ export class UploadFileService {
 
   getFiles(): Observable<any> {
     return this.http.get('http://localhost:8080/api/file/info');
+  }
+
+  getAllFiles(): Observable<any> {
+    return this.http
+      .get<any>('http://localhost:8080/api/file/info')
+      .pipe(
+        retry(2),
+        catchError(this.handleError)
+      )
   }
 }
